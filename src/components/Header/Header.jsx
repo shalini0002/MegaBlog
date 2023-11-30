5dc80ecbd3d75f9f57b6afe3e7e92fed7d43b8a9
Shalini <shalinisharmar60@gmail.com>
2023-11-30 20:41:13 +0530

    ui changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Container, Logo, LogoutBtn } from "../index";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -7,7 +7,12 @@ import { useNavigate } from "react-router-dom";
 function Header() {
   const authStatus = useSelector((state) =>  state.auth.status);
   const navigate = useNavigate();
-  // console.log(item.length);
+  // const [isActive, setIsActive] = useState(false);
+  // const toggleActive = () => {
+  //   setIsActive(!isActive);
+  // };
+  // const activeLink = ' bg-blue-100 text-black'
+  // const normalLink = ''
 
   const navItems = [
     {
@@ -38,21 +43,26 @@ function Header() {
   ];
 
   return (
-    <header className="py-3 shadow bg-gray-500">
+    <header className="py-3 shadow bg-purple-700 w-full flex justify-between items-center px-10 rounded-full border border-black drop-shadow-solid">
       <Container>
-        <nav className="flex">
+        <nav className="flex align-items-center">
           <div className="mr-4">
             <Link to="/">
               <Logo width="70px" />
             </Link>
           </div>
-          <ul className="flex ml-auto">
+          <ul className="flex ml-auto md:flex gap-4 lg:gap-6 justify-around items-center">
             {navItems.map((item) =>
               item.active ? (
                 <li key={item.name}>
                   <button
-                    onClick={() => navigate(item.slug)}
-                    className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                    onClick={() => navigate(item.slug) }
+                    className="text-white active:text-black"
+                    //className={`p-4 ${{isActive} ? 'text-black' : 'text-white'}`}
+                    // className="inline-bock px-6 py-2 duration-200 hover:bg-pink-400
+                    //  rounded-full text-white font-semibold transition-all text-xs
+                    //   uppercase hover:no-underline drop-shadow-solid border border-black
+                    //    hover:drop-shadow-solid-extend"
                   >
                     {item.name}
                   </button>
